6addffddecc61151acb07242e0e9dd57701cc428
lycreate <2598932509@qq.com>
2021-11-21 10:13:17 +0800

    .

---
+++ 2021-11-21/src/Static.java
@@ -0,0 +1,28 @@
+public class Static {
+//    static int a = print();
+//    static {
+//        System.out.println("hello");
+//    }
+//
+//    public static int print() {
+//        System.out.println("666");
+//        return 0;
+//    }
+//    public static void main(String[] args) {
+//
+//        System.out.println("a");
+//    }
+//
+    public static void printHelle() {
+        System.out.println("hello");
+    }
+    private static void printWorld() {
+        System.out.println("world");
+    }
+    public static int a = 5;
+    private static int b = 10;
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ 2021-11-21/src/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        Static.printHelle();
+        System.out.println(Static.a);
+    }
+}
