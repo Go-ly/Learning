9e097378779deb5226f65c131adb642507c00453
lycreate <2598932509@qq.com>
2022-01-03 16:45:22 +0800

    .

--- 20212-1-2 类和对象/src/Practice.java
+++ 20212-1-2 类和对象/src/Practice.java
@@ -3,14 +3,30 @@ class Dog {
     public int age;
     public boolean are;
     public static int a;
+//    public Dog() {
+//
+//    }
+    public Dog(String name) {
+        this.name = name;
+    }
     public void bark() {
         System.out.println("汪汪");
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", are=" + are +
+                '}';
     }
 }
 
 public class Practice {
     public static void main(String[] args) {
-        Dog dog = new Dog();   // 实例化一个对象
-        System.out.println(dog.a);
+        Dog dog = new Dog("旺财");
+        System.out.println(dog.name);
+        System.out.println(dog);
     }
 }
