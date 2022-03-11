da4a682334dd7d6edb7b98566e07fbe5eb4e6145
liyang <2598932509@qq.com>
2022-03-11 11:03:13 +0800

    .

---
+++ 2022-3-11 implements_MyArrayList/src/MyStack.java
@@ -0,0 +1,191 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Deque;
+import java.util.Iterator;
+
+public class MyStack<E> implements Deque<E> {
+    // 以顺序表形式去实现   组合
+    private final ArrayList<E> list = new ArrayList<>();
+    @Override
+    public E peek() {
+        return list.get(list.size() - 1);
+    }
+
+    @Override
+    public void push(E e) {
+        list.add(e);
+    }
+
+    @Override
+    public E pop() {
+        return list.remove(list.size() - 1);
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+
+    // ------------------------------------------------------
+
+
+    @Override
+    public void addFirst(E e) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addLast(E e) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean offerFirst(E e) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean offerLast(E e) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E removeFirst() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E removeLast() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E pollFirst() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E pollLast() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E getFirst() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E getLast() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E peekFirst() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E peekLast() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeFirstOccurrence(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeLastOccurrence(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean add(E e) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean offer(E e) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E remove() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E poll() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E element() {
+        throw new UnsupportedOperationException();
+    }
+
+
+
+    @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+
+
+    @Override
+    public Iterator<E> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object[] toArray() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<E> descendingIterator() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ 2022-3-11 implements_MyArrayList/src/MyStack2.java
@@ -0,0 +1,203 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class MyStack2<E> implements Deque<E> {
+    // 以顺序表形式去实现
+    private E[] array;
+    private int size;
+
+    public MyStack2() {
+        array = (E[]) new Object[10];
+        size = 0;
+    }
+
+    @Override
+    public E peek() {
+        return array[size - 1];
+    }
+
+    @Override
+    public void push(E e) {
+        if (size == array.length) {
+            array = Arrays.copyOf(array, 2 * array.length);
+        }
+        array[size] = e;
+        size++;
+    }
+
+    @Override
+    public E pop() {
+        E e = array[size - 1];
+        array[size - 1] = null;
+        size--;
+        return e;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+    // ------------------------------------------------------
+
+
+    @Override
+    public void addFirst(E e) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addLast(E e) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean offerFirst(E e) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean offerLast(E e) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E removeFirst() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E removeLast() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E pollFirst() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E pollLast() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E getFirst() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E getLast() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E peekFirst() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E peekLast() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeFirstOccurrence(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeLastOccurrence(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean add(E e) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean offer(E e) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E remove() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E poll() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E element() {
+        throw new UnsupportedOperationException();
+    }
+
+
+
+    @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+
+
+    @Override
+    public Iterator<E> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object[] toArray() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<E> descendingIterator() {
+        throw new UnsupportedOperationException();
+    }
+}
